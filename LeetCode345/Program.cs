fc3abbe6d54a5f6ac3e7a5cc2377fc1d4aa40cf0
Carter O'Leary <carter.m.oleary@outlook.com>
2025-05-01 11:38:45 -0400

    Add files via upload

---
+++ LeetCode345/Program.cs
@@ -0,0 +1,33 @@
+﻿string[] strings = { "IceCream", "leetcode"};
+foreach (string s in strings)
+{
+    Console.WriteLine(ReverseVowels(s));
+}
+
+string ReverseVowels(string s)
+{
+    var charIndices = new List<int>();
+    string lower = s.ToLower();
+    char[] reverse = s.ToCharArray();
+    for (int i = 0; i < lower.Length; i++)
+    {
+        switch (lower[i])
+        {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                charIndices.Add(i);
+                continue;
+            default: continue;
+        }
+    }
+    for(int i = 0; i < charIndices.Count / 2; i++)
+        {
+        char temp = reverse[charIndices[i]];
+        reverse[charIndices[i]] = reverse[charIndices[charIndices.Count - 1 - i]];
+        reverse[charIndices[charIndices.Count - 1 - i]] = temp;
+    }
+    return new string(reverse);
+}
