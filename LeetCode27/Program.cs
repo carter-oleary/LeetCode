988490e06cfe5bd3f04f9900dd376345bbbe2783
carter-oleary <carter.m.oleary@outlook.com>
2025-05-06 16:49:41 -0400

    LeetCode27

---
+++ LeetCode27/Program.cs
@@ -0,0 +1,23 @@
+﻿int[] nums = { 3, 2, 2, 3 };
+int val = 3;
+int k = RemoveElement(nums, val);
+Console.WriteLine($"There are {k} elements that are not {val}");
+for(int i = 0; i < k; i++)
+{
+    Console.Write(nums[i]+ " ");
+}
+
+
+int RemoveElement(int[] nums, int val)
+{
+    var numList = new List<int>();
+    foreach (int num in nums)
+    {
+        if(!(num == val)) numList.Add(num);
+    }
+    for (int i = 0; i < numList.Count; i++)
+    {
+        nums[i] = numList[i];
+    }
+    return numList.Count;
+}
