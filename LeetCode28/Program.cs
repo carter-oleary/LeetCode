36cd14b07d24d45187e86fcc8ea4cfae9205bb7f
carter-oleary <carter.m.oleary@outlook.com>
2025-05-06 16:55:53 -0400

    LeetCode28

---
+++ LeetCode28/Program.cs
@@ -0,0 +1,12 @@
+﻿int StrStr(string haystack, string needle)
+{
+    for (int i = 0; i < haystack.Length - needle.Length + 1; i++)
+    {
+        if (haystack[i] == needle[0])
+        {
+            string sub = haystack.Substring(i, needle.Length);
+            if (sub == needle) return i;
+        }
+    }
+    return -1;
+}
