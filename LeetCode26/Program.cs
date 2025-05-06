b5aa3cd83a435ca72fd877b3d73cf95cc99d449e
carter-oleary <carter.m.oleary@outlook.com>
2025-05-06 16:42:31 -0400

    LeetCode 26

---
+++ LeetCode26/Program.cs
@@ -0,0 +1,20 @@
+﻿int[] arr = { 1, 1, 2 };
+int k = RemoveDuplicates(arr);
+for(int i = 0; i < k; i++)
+{
+    Console.Write(arr[i] + " ");
+}
+
+int RemoveDuplicates(int[] arr)
+{
+    List<int> result = new List<int>();
+    foreach (int value in arr)
+    {
+        if(!result.Contains(value)) result.Add(value);
+    }
+    for(int i = 0; i < result.Count; i++)
+    {
+        arr[i] = result[i];
+    }
+    return result.Count;
+}
