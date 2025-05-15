defd9b9c242ef8764e3ac5614055c1a23fa5b38b
carter-oleary <carter.m.oleary@outlook.com>
2025-05-15 14:59:24 -0400

    LeetCode 566

---
+++ LeetCode566_ReshapeMatrix/Program.cs
@@ -0,0 +1,28 @@
+﻿int[][] mat = [[1, 2], [3, 4]];
+int[][] res = Reshape(mat, 2, 2);
+
+int[][] Reshape(int[][] mat, int r, int c)
+{
+    int m = mat.Length;
+    int n = mat[0].Length;
+    if (m * n != r * c) return mat; //Check edge case
+    int[][] reshaped = new int[r][];
+    for (int i = 0; i < r; i++) { reshaped[i] = new int[c]; } //Initializes new matrix
+                                                              //Tracks reshaped row and column
+    int rowPointer = 0;
+    int colPointer = 0;
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            reshaped[rowPointer][colPointer] = mat[i][j];
+            colPointer++;
+            if (colPointer == c)
+            { //Reset to beginning of next row
+                colPointer = 0;
+                rowPointer++;
+            }
+        }
+    }
+    return reshaped; ;
+}
